97755cb3c2b2cd78468a2e75a4bbde8be9fdd701
Tegisty Hailay <tigisthay13@gmail.com>
2022-10-30 06:59:32 +0300

    frontend improvment for a contract

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,9 +1,4 @@
 import abi from './Employer.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0xCFfed15b5BBE7b7EC062dbbC10EC9dffe3231d61';
-
-// '0xfE44528D7D82De7628433644B2baE10db2652925'  
-// '0xfB3cbcA703e9022c691AE7dFF0F6627285b95FeE'  
-// '0x47Ec2ff576868962836a3CA7818e5C2f657cd8db'
-// '0xB699b614B5F8D61e7498fEbe845Eef222FecfCdC' 
+export const contractAddress = '0x423CDF9E217ccADAe24C0fde562Ac8A897A177B8';
